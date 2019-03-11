fc188b4b1ed8b41c01b5bd55b312caafa399cc40
mattvil <mattvilain@gmail.com>
2019-03-11 15:41:04 -0700

    split dataset for K-fold crossvalidation

--- naiveBayes.py
+++ naiveBayes.py
@@ -1,21 +1,28 @@
+from sklearn.model_selection import train_test_split
 
 class NaiveBayes:
 
     def __init__(self):
-        pass
+        self.probabilities = []
 
-    def training(self, nbIter):
-        for iter in range(nbIter):
-            self.__Kfold_cross_validation()
-        pass
+    def training(self, data, K, trainRatio=0.5, validRatio=0.3, testRatio=0.2):
+        """"""
+        self.__buildProbabilities()
+        self.__Kfold_cross_validation(data, K)
 
-    def __Kfold_cross_validation(self, data, trainRatio=0.5, validRatio=0.3, testRatio=0.1):
+    def __Kfold_cross_validation(self, data, K, trainRatio=0.5, validRatio=0.3, testRatio=0.2):
         """split dataset + compute classes probabilities"""
-        xTrain, xValid, xTest = self.__split_dataset(data,
-                                                     trainRatio,
-                                                     validRatio,
-                                                     testRatio)
+        train, test = train_test_split(data, test_size=testRatio)
+
+        for k in range(K):
+            xTrain, xValid = self.__splitDataset(train)
+            print("{}:\tTrain: {}\tVaildation: {}\tTest: {}".format(k, len(xTrain), len(xValid), len(test)))
+
+
+    def __buildProbabilities(self):
+        """"""
         pass
 
-    def __split_dataset(self, data, trainRatio=0.5, validRatio=0.3, testRatio=0.1):
-        pass
+    def __splitDataset(self, data, trainRatio=0.7, validRatio=0.3):
+        """"""
+        return train_test_split(data, test_size=validRatio)
--- test.py
+++ test.py
@@ -5,17 +5,23 @@ from preprocessor import Precessor
 from naiveBayes import NaiveBayes
 
 PATH_DATA = "./data/adult.data"
-DATASET_COLUMNS = ["age","workclass","fnlwgt","education","education_num","marital_status","occupation","relationship","race","sex","capital_gain","capital_loss","hours_per_week","native_country","income"]
+DATASET_COLUMNS = ["age","workclass","fnlwgt","education","education_num",
+                   "marital_status","occupation","relationship","race","sex",
+                   "capital_gain","capital_loss","hours_per_week",
+                   "native_country","income"]
 
 
 def loadData():
-    return pd.read_csv(PATH_DATA, names=DATASET_COLUMNS, sep=", ")
+    return pd.read_csv(PATH_DATA,names=DATASET_COLUMNS,sep=", ",engine='python')
 
 def main():
     data = loadData()
-    print(data)
     preprocessor = Precessor()
     cleanDataset = preprocessor.preprocess(data)
+    print(cleanDataset.info())
+
+    naiveBayesModel = NaiveBayes()
+    naiveBayesModel.training(cleanDataset, 10)
 
 
 
